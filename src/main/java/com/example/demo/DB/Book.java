71b8c222878e8ceba03e672e83f115c060bcdbc0
Damian <damian1003@onet.pl>
2025-05-16 17:57:21 +0200

    zrobione 3 zadanie

--- src/main/java/com/example/demo/DB/Book.java
+++ src/main/java/com/example/demo/DB/Book.java
@@ -5,9 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter @NoArgsConstructor @AllArgsConstructor
 public class Book {
@@ -22,4 +28,9 @@ public class Book {
 
     @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
     private List<Review> reviews;
+
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime modified;
 }
